603a84a42e6257aa4be8f2e2edfecd9f79f07460
kimyou1102 <yoosk5485@naver.com>
2022-12-21 23:27:05 +0900

    feat: 임시데이터 달력에 렌더링

--- src/CalendarMain.jsx
+++ src/CalendarMain.jsx
@@ -1,37 +1,88 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import {
   Main,
   DatesDiv,
   DateWrap,
   DateDiv,
   DateText,
+  OpenTitleWrap,
+  OpenTitleDiv,
+  OpenTitle,
 } from "./styledComponents";
 
-const CalendarMain = ({ lastDays, currentDays, nextDays }) => {
+const CalendarMain = ({
+  year,
+  month,
+  lastDays,
+  currentDays,
+  nextDays,
+  performanceData,
+}) => {
+  useEffect(() => {
+    setTimeout(() => {}, 2000);
+  }, []);
+
   return (
     <Main>
       <DatesDiv>
-        {lastDays.map((date, index) => (
-          <DateWrap key={index}>
-            <DateDiv opacity="0.3">
-              <DateText>{date}</DateText>
-            </DateDiv>
-          </DateWrap>
-        ))}
-        {currentDays.map((date, index) => (
-          <DateWrap key={index}>
-            <DateDiv>
-              <DateText>{date}</DateText>
-            </DateDiv>
-          </DateWrap>
-        ))}
-        {nextDays.map((date, index) => (
-          <DateWrap key={index}>
-            <DateDiv opacity="0.4">
-              <DateText>{date}</DateText>
-            </DateDiv>
-          </DateWrap>
-        ))}
+        {lastDays.map((day, index) => {
+          const date = `${month !== 1 ? year : year - 1}.${
+            month === 1 ? 12 : month < 10 ? `0${month - 1}` : month - 1
+          }.${day > 9 ? day : `0${day}`}`;
+          return (
+            <DateWrap key={index} id={date}>
+              <DateDiv opacity="0.3">
+                <DateText>{day}</DateText>
+                {performanceData[date] !== undefined ? (
+                  <OpenTitleWrap>
+                      <OpenTitleDiv>
+                      <OpenTitle>{performanceData[date].title}</OpenTitle>
+                    </OpenTitleDiv>
+                  </OpenTitleWrap>
+                  
+                ) : null}
+              </DateDiv>
+            </DateWrap>
+          );
+        })}
+        {currentDays.map((day, index) => {
+          const date = `${year}.${String(month).padStart(2, 0)}.${
+            day > 9 ? day : `0${day}`
+          }`;
+          return (
+            <DateWrap key={index} id={date}>
+              <DateDiv>
+                <DateText>{day}</DateText>
+                {performanceData[date] !== undefined ? (
+                  <OpenTitleWrap>
+                      <OpenTitleDiv>
+                      <OpenTitle>{performanceData[date].title}</OpenTitle>
+                    </OpenTitleDiv>
+                  </OpenTitleWrap>
+                ) : null}
+              </DateDiv>
+            </DateWrap>
+          );
+        })}
+        {nextDays.map((day, index) => {
+          const date = `${month === 12 ? year + 1 : year}.${
+            month === 12 ? 1 : month < 10 ? `0${month + 1}` : month + 1
+          }.${day > 9 ? day : `0${day}`}`;
+          return (
+            <DateWrap key={index} id={date}>
+              <DateDiv opacity="0.4">
+                <DateText>{day}</DateText>
+                {performanceData[date] !== undefined ? (
+                  <OpenTitleWrap>
+                      <OpenTitleDiv>
+                      <OpenTitle>{performanceData[date].title}</OpenTitle>
+                    </OpenTitleDiv>
+                  </OpenTitleWrap>
+                ) : null}
+              </DateDiv>
+            </DateWrap>
+          );
+        })}
       </DatesDiv>
     </Main>
   );
