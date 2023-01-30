8f87a9e29f48bec8a803f2ff3b307313c2a5d271
kimyou1102 <yoosk5485@naver.com>
2023-01-31 00:54:38 +0900

    feat: 입력한 값 api에 추가

--- src/MusicianAdd.jsx
+++ src/MusicianAdd.jsx
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { MusicianAddInput, AddBtn } from './styledComponents';
+import axios from 'axios';
 
 const MusicianAdd = () => {
+    const [value, setValue] = useState('');
+    const submit = (e) => {
+        e.preventDefault();
+        if(value === '') return;
+
+        axios.post('http://127.0.0.1:8000/api/artist', {
+            name: value
+        })
+        .then((res) => {
+            console.log(res.data);
+        })
+        .catch((error) => {
+            console.log(error);
+        })
+        setValue('');
+    }
+
     return (
         <div>
             <h1>가수들 추가하는 페이지</h1>
             <Link to="/calendar">
                 <h2>공연예매 달력 보러 가기🌹</h2>
             </Link>
-            <MusicianAddInput/>
-            <AddBtn width={'50%'}>추가하기</AddBtn>
+            <form onSubmit={submit}>
+                <MusicianAddInput value={value} onChange={(e) => {
+                    setValue(e.target.value);
+                }}/>
+                <AddBtn width={'50%'}>추가하기</AddBtn>
+            </form>
+            <br></br>           
         </div>
     );
 };
